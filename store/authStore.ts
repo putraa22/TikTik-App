94e4b7176b767d8a46c41b8997ba6bfa78401f0f
Hermansyah Putra <hermansyahputra0418@gmail.com>
2022-10-30 20:31:43 +0700

    Finish Fitur Comment

--- store/authStore.ts
+++ store/authStore.ts
@@ -6,9 +6,16 @@ import { BASE_URL } from "../utils";
 
 const authStore = (set: any) => ({
     userProfile: null,
+    allUsers: [],
 
     addUser: (user : any) => set({userProfile: user}),
-    removeUser: () => set({userProfile: null})
+    removeUser: () => set({userProfile: null}),
+
+    fetchAllUsers: async () => {
+        const response = await axios.get(`${BASE_URL}/api/users`);
+
+        set({allUsers: response.data});
+    }
 });
 
 const useAuthStore = create(
